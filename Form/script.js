7c499a493a50daa137255e8d40257f546b5a62d3
baharihari49 <haribahari71@gmail.com>
2022-11-07 21:57:30 +0700

    perubahan kedua

---
+++ Form/script.js
@@ -0,0 +1,30 @@
+// const email = document.querySelector('.test-email')
+
+// email.addEventListener('click', kirim)
+
+// // function kirim() {
+// //     return Email.send({
+// //         Host : "smtp.elasticemail.com",
+// //         Username : "baharihari49@gmail.com",
+// //         Password : "18EC6443E0B39C61D9A192263E0854E683E6",
+// //         To : 'baharihari49@gmail.com',
+// //         From : "baharihari49@gmail.com",
+// //         Subject : "Coba",
+// //         Body : "apakah terkirim"
+// //     }).then(
+// //       message => alert(message)
+// //     );
+// // }
+// function kirim() {
+//     return Email.send({
+//         SecureToken : "c8ed59d7-6924-4cfd-87b2-1137b941954d",
+//         To : 'baharihari49@gmail.com',
+//         From : "zulfikri@umsu.ac.id",
+//         Subject : "Coba",
+//         Body : "apakah terkirim"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+
+// console.log(kirim());
